2f84c25421b89eeaff38611c94169a149befb6db
Cleisson Vieira <cleissonvb@gmail.com>
2016-11-09 06:25:34 -0300

    tudo o que foi feito ate agora

---
+++ www/js/app.js
@@ -0,0 +1,68 @@
+// Initialize Firebase
+var config = {
+    apiKey: "AIzaSyCYP1BQlG9CZbaUfhrQE4GdFl9SNkXl3mg",
+    authDomain: "teste-incrivel.firebaseapp.com",
+    databaseURL: "https://teste-incrivel.firebaseio.com",
+    storageBucket: "teste-incrivel.appspot.com",
+    messagingSenderId: "936131680484"
+};
+firebaseConnection = firebase.initializeApp(config);
+
+var categoriaDAO;
+var produtoDAO;
+var itemCompraDAO;
+var listaCategorias;
+var listaProdutos;
+var listaItensCompras;
+
+function logar(){
+    login = document.getElementById("login").value;
+    senha = document.getElementById("senha").value;
+    userDAO = firebaseConnection.database().ref('/users/'+login);
+    userDAO.once('value').then(function(snapshot) {
+        if(snapshot.val() != null){
+            base = snapshot.val().base;
+            categoriaDAO = firebaseConnection.database().ref('/'+base+'/categorias/');
+            categoriaDAO.once('value').then(function(s) {
+                listaCategorias = s.val();
+                console.log(listaCategorias);
+              });
+            produtoDAO = firebaseConnection.database().ref('/'+base+'/produtos/');
+            produtoDAO.once('value').then(function(s) {
+              listaProdutos = s.val();
+              console.log(listaProdutos);
+          }).then();
+            itemCompraDAO = firebaseConnection.database().ref('/'+base+'/itens_compras/');
+            itemCompraDAO.once('value').then(function(s) {
+                listaItensCompras = s.val();
+                console.log(listaItensCompras);
+              });
+        }else{
+            console.log("USER NOT ENCONTRADO");
+        }
+
+    }, function(error) {
+      console.error(error);
+    });
+}
+
+function pesquisarCategoriaById(id){
+    c = categoriaDAO.child(id);
+    c.once('value').then(function(snapshot) {
+        return snapshot.val();
+    });
+}
+
+function pesquisarProdutoById(id){
+    p = produtoDAO.child(id);
+    p.once('value').then(function(snapshot) {
+        return snapshot.val();
+    });
+}
+
+function pesquisarItemCompraById(id){
+    i = itemCompraDAO.child(id);
+    i.once('value').then(function(snapshot) {
+        return snapshot.val();
+    });
+}
---
+++ www/js/nav.js
@@ -0,0 +1,9 @@
+function nav(page){
+    document.getElementById("content").innerHTML='<div w3-include-html="'+page+'.html"></div>';
+
+    w3IncludeHTML();
+}
+function carregarPaginaProdutos(){
+    alert('chamou');
+
+}
